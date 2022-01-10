5e90970a13b62e32887d6de5548be0998d9ed1c2
Antônio <aclcampos@live.com>
2022-01-10 18:10:40 -0300

    Updates

---
+++ precache-manifest.a0aaa6d82cbbf656327472b399d714fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8105efca8c3819509fda865f420e47ac",
+    "url": "/movie-cards-library/index.html"
+  },
+  {
+    "revision": "e2a3b6211ae886567107",
+    "url": "/movie-cards-library/static/css/main.a8a6561d.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/movie-cards-library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "e2a3b6211ae886567107",
+    "url": "/movie-cards-library/static/js/main.06ab0878.chunk.js"
+  },
+  {
+    "revision": "a95db5ba2439b13bdc08",
+    "url": "/movie-cards-library/static/js/runtime-main.a37d5318.js"
+  }
+]);
